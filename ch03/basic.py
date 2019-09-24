00f6288b343f9234d02bd6e74892cff7eb151002
Sahil Sapolia <sahilsapolia@gmail.com>
2019-09-24 10:07:37 -0700

    first program

---
+++ ch03/basic.py
@@ -0,0 +1,5 @@
+import sys
+print ('Hello and Welcome to basic python program')
+print('Hello, ' + sys.argv[1] + ' and Welcome to basic python program')
+food = input("What is your favourite food?")
+print("you like " + food + "? I might like it too")
